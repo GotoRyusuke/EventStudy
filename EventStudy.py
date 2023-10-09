2d8ea33874829e5906f726c63f53632b51d742ce
後藤龍介 <94293167+GotoRyusuke@users.noreply.github.com>
2023-10-09 16:40:15 +0800

    Revision
    EventStudy class now allows rolling estimation period.

--- EventStudy.py
+++ EventStudy.py
@@ -1,4 +1,22 @@
 # -*- coding: utf-8 -*-
+'''
+Description
+------------
+A class to conduct event study. Specifically, it can generate CARs for each event, 
+which in this case is the earnings conference calls.
+
+Structure
+---------
+- <CLASS> EventStudy
+    - <METHOD> __init__
+    - <METHOD> gen_car
+        Method to generate CAR for a given event for a given stock
+    - <METHOD> gen_tic_car_df
+        Method to generate CARs for a given stock for all events
+    - <METHOD> run
+        Method to generate CARs for all stocks for all events
+
+'''
 import pandas as pd
 import numpy as np
 from tqdm import tqdm
@@ -14,29 +32,42 @@ class EventStudy:
             evt_panel_dir,
             stock_data,
             market_data,
-            est_period,
-            evt_window=1,
+            est_period=(None, None),
+            evt_window=3,
+            rolling=True,
+            rolling_window=(-65, -2),
             gen_returns=False,
             ):
-        self.stock_data = stock_data
-        self.market_data = market_data
-        self.full_period = list(stock_data.index)
+        self.stock_data = stock_data.set_index('date')
+        self.market_data = market_data.set_index('date')
+        self.full_period = stock_data['date'].to_list()
         self.evt_panel = pd.read_csv(evt_panel_dir)
         
         # preparation for estimation
-        self.est_start, self.est_end = est_period
+        # if est_period is not None, then use est_period as estimation period
+        # else use the rolling estimation period
+        if est_period == (None, None):
+            if rolling:
+                self.est_start, self.est_end = rolling_window
+            else:
+                raise ValueError('est_period is None and rolling is False')
+        else:
+            self.est_start, self.est_end = est_period
         
         # preparation for event
         self.evt_window = evt_window
 
+        # preparation for returns
+        # if gen_returns is True, then generate returns from stock_data and market_data
         if gen_returns:
-            self.actual_returns = EventUtils.gen_daily_returns(self.stock_data)
+            self.stock_returns = EventUtils.gen_daily_returns(self.stock_data)
+            self.market_returns = EventUtils.gen_daily_returns(self.market_data)
         else:
-            self.actual_returns = stock_data
-        self.market_returns = EventUtils.gen_daily_returns(self.market_data)
+            self.stock_returns = stock_data.set_index('date')
+            self.market_returns = market_data.set_index('date')
     
     def gen_car(self, tic, evt_date):
-        stock_returns = self.actual_returns[tic]
+        stock_returns = self.stock_returns[tic]
         market_returns = self.market_returns
 
         if evt_date not in self.full_period:
@@ -45,18 +76,32 @@ class EventStudy:
                 evt_date = evt_date.strftime('%Y-%m-%d')
         
         evt_start = self.full_period[
-            self.full_period.index(evt_date) - self.evt_window
+            self.full_period.index(evt_date) - int((self.evt_window -1) / 2)
             ]
         evt_end = self.full_period[
-            self.full_period.index(evt_date) + self.evt_window
+            self.full_period.index(evt_date) + int((self.evt_window -1) / 2) 
             ]
+        # print(evt_start)
+        # print(evt_end)
         
         # get expected returns
+        #      Estimation (63 days)  Event (window days)
+        # |-----------------------|----|---------|
+        # est_start         est_end  evt_start evt_end
+        est_start = self.full_period[
+            self.full_period.index(evt_date) + self.est_start + 1
+        ]
+        est_end = self.full_period[
+            self.full_period.index(evt_date) + self.est_end
+        ]
+        # print(est_start)
+        # print(est_end)
+
         beta, intercept = EventUtils.apply_market_model(
             stock_returns,
             market_returns,
-            est_start=self.est_start,
-            est_end=self.est_end,
+            est_start=est_start,
+            est_end=est_end,
             )
         
         # get AR
--- EventUtils.py
+++ EventUtils.py
@@ -1,17 +1,25 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 import pandas as pd
-from tqdm import tqdm
 from scipy import stats
-
-
-
 
 # df = pd.read_csv('C:\\Users\\76782\\Desktop\\DATA_SP1500_close.csv')
 # df.set_index('index', inplace=True)
 
-# calculate daily returns
 def gen_daily_returns(data, base=1):
+    '''
+    Description
+    ------------
+    A function to calculate daily returns from a given data
+    
+    Args
+    ----
+        data: a numpy array, a pandas series or a pandas dataframe
+        base: the base of the returns, default is 1
+    Returns
+    -------
+        A numpy array, a pandas series or a pandas dataframe of daily returns
+    '''
     if type(data) is np.ndarray:
         daily_returns = np.zeros(shape=data.shape)
         daily_returns[base:] = data[base:] / data[0:-base] - 1
@@ -24,21 +32,39 @@ def gen_daily_returns(data, base=1):
         returns = data.apply(gen_daily_returns)
         returns.index=data.index
         return returns
-
 # returns = gen_daily_returns(df)
 
-# calcualte expected returns
 def apply_market_model(
         stock_returns, 
         market_returns,
         est_start,
         est_end,
         ):
+    '''
+    Description
+    ------------
+    A function to apply market model to a given stock and market returns
+
+    Args
+    ----
+        stock_returns: a numpy array, a pandas series or a pandas dataframe
+        market_returns: a numpy array, a pandas series or a pandas dataframe
+        est_start: str
+            the start date of the estimation period
+        est_end: str
+            the end date of the estimation period
     
+    Returns
+    -------
+        beta: float
+            the estimated beta
+        intercept: float
+            the estimated intercept
+    '''
     # prepare data
     stock_returns = gen_period_data(stock_returns, est_start, est_end)
     market_returns = gen_period_data(market_returns, est_start, est_end)
-        
+    
     # estimation
     y = np.array(stock_returns)
     x = np.array(market_returns)
@@ -47,14 +73,31 @@ def apply_market_model(
     # print(f'Estimated Beta: {beta}')
     # print(f'Estimated Intercept: {intercept}')
     # print(f'Estimation metrics:\n\tp-value: {p}\n\tStd Error: {std}')
-    
     return beta, intercept
 
 def gen_period_data(data, start_date, end_date):
+    '''
+    Description
+    ------------
+    A function to generate a period of data from a given data
+
+    Args
+    ----
+        data: a numpy array, a pandas series or a pandas dataframe
+        start_date: str
+            the start date of the period
+        end_date: str
+            the end date of the period
+    
+    Returns
+    -------
+        A numpy array, a pandas series or a pandas dataframe of the period data
+    '''
+    if type(data) is pd.DataFrame:
+        data = data['close']
     dates = list(data.index)
     start_idx = dates.index(start_date)
     end_idx = dates.index(end_date)
     
     return data[start_idx:end_idx+1]
-
 # test = gen_period_data(returns['AAPL.O'], '2022-01-03', '2023-01-06')
